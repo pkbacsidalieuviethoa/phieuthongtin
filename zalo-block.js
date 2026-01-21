606c334b0ccc416e82b94c2bde031f98176d4e1c
pkbacsidalieuviethoa <pkbacsidalieuviethoa@gmail.com>
2026-01-21 18:37:48 +0700

    Update zalo-block.js

--- zalo-block.js
+++ zalo-block.js
@@ -1,9 +1,15 @@
 (function () {
     var ua = navigator.userAgent.toLowerCase();
-    var isZalo = ua.indexOf("zalo") !== -1;
+
+    var isZalo =
+        ua.includes("zalo") ||
+        ua.includes("zaloandroid") ||
+        ua.includes("zaloios") ||
+        (ua.includes("android") && ua.includes("wv")) || // Android WebView
+        (ua.includes("iphone") && !ua.includes("safari")); // iOS WebView
 
     if (isZalo) {
         sessionStorage.setItem("redirect_after_browser", window.location.href);
-        window.location.replace("zalo-block.html");
+        window.location.href = "zalo-block.html";
     }
 })();
